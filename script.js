2e7f856136fbb17d47f0cebe8dd68a603fb22db0
ANTALAN <>
2022-12-15 20:34:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = prompt("видите вашь возрост")
+if (typeof (a) == 'object') {
+  while (typeof (a) == 'object') {
+    a = prompt('ПИДРИЛА БЛЯ ВВЕДИ ВОЗРАСТ')
+  }
+}
+if (a > 18) { alert("вам павестка") }
+else { alert("тебе пока повезло") }
